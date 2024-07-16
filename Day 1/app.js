56886eb1026a9653bf2f236273f664cceac10e39
Faizan Khan <faizikhan753@gmail.com>
2024-07-16 20:31:16 +0500

    30 days

---
+++ Day 1/app.js
@@ -0,0 +1,64 @@
+// Task 1
+var number = 20;
+console.log("This is Number", number);
+
+let string = "Hello";
+console.log("This is String", string);
+
+// Task 2
+const boolean = true;
+console.log("This is Boolean", boolean);
+
+// Task 3
+let num = 123
+let str = "Hello"
+let bool = false;
+let obj = {name: "Faizan", age: 27}
+let arr = [1, 2, 3, 4, 5]
+
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof bool);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Task 4
+let reassign = "initial value"
+console.log("This is Reassigining variable", reassign)
+
+reassign = "new value"
+console.log("This is new Reassigining variable", reassign);
+
+// Task 5
+const decVar = "declared value";
+console.log("This is Reassigining variable", decVar);
+
+// Feature Request
+
+// 1
+let numVar = 123;
+let strVar = "Hello";
+let boolVar = false;
+let objVar = { location: "Karachi"};
+let arrVar = [1, 2, 3, 4, 5];
+
+console.log("Value", numVar, typeof numVar);
+console.log("Value", strVar, typeof strVar);
+console.log("Value", boolVar, typeof boolVar);
+console.log("Value", objVar, typeof objVar);
+console.log("Value", arrVar, typeof arrVar); 
+
+// 2
+let reassignLet = "initial value"
+console.log("This is Reassigining variable", reassignLet)
+
+reassignLet = "new value"
+console.log("This is new Reassigining variable", reassignLet);
+
+const reassignConst = "declared value";
+console.log("This is Reassigining variable", reassignConst);
+
+
+
+
+
